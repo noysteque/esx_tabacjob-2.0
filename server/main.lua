5ac864699a4b9a804578267b23e3b67e5c5bab10
Bruno <samguegvideos@gmail.com>
2018-08-10 20:54:43 +0200

    Update

---
+++ server/main.lua
@@ -0,0 +1,329 @@
+-----------------------------------------
+-- Created and modify by L'ile Légale RP
+-- SenSi and Kaminosekai
+-----------------------------------------
+
+ESX = nil
+local PlayersTransforming  = {}
+local PlayersSelling       = {}
+local PlayersHarvesting = {}
+local tabacsec = 1
+local jus = 1
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+if Config.MaxInService ~= -1 then
+	TriggerEvent('esx_service:activateService', 'tabac', Config.MaxInService)
+end
+
+TriggerEvent('esx_phone:registerNumber', 'tabac', _U('tabac_client'), true, true)
+TriggerEvent('esx_society:registerSociety', 'tabac', 'tabac', 'society_tabac', 'society_tabac', 'society_tabac', {type = 'private'})
+local function Harvest(source, zone)
+	if PlayersHarvesting[source] == true then
+
+		local xPlayer  = ESX.GetPlayerFromId(source)
+		if zone == "feuilletabFarm" then
+			local itemQuantity = xPlayer.getInventoryItem('feuilletab').count
+			if itemQuantity >= 100 then
+				TriggerClientEvent('esx:showNotification', source, _U('not_enough_place'))
+				return
+			else
+				SetTimeout(1800, function()
+					xPlayer.addInventoryItem('feuilletab', 1)
+					Harvest(source, zone)
+				end)
+			end
+		end
+	end
+end
+
+RegisterServerEvent('esx_tabacjob:startHarvest')
+AddEventHandler('esx_tabacjob:startHarvest', function(zone)
+	local _source = source
+  	
+	if PlayersHarvesting[_source] == false then
+		TriggerClientEvent('esx:showNotification', _source, '~r~C\'est pas bien de glitch ~w~')
+		PlayersHarvesting[_source]=false
+	else
+		PlayersHarvesting[_source]=true
+		TriggerClientEvent('esx:showNotification', _source, _U('feuilletab_taken'))  
+		Harvest(_source,zone)
+	end
+end)
+
+
+RegisterServerEvent('esx_tabacjob:stopHarvest')
+AddEventHandler('esx_tabacjob:stopHarvest', function()
+	local _source = source
+	
+	if PlayersHarvesting[_source] == true then
+		PlayersHarvesting[_source]=false
+		TriggerClientEvent('esx:showNotification', _source, 'Vous sortez de la ~r~zone')
+	else
+		TriggerClientEvent('esx:showNotification', _source, 'Vous pouvez ~g~récolter')
+		PlayersHarvesting[_source]=true
+	end
+end)
+
+
+local function Transform(source, zone)
+
+	if PlayersTransforming[source] == true then
+
+		local xPlayer  = ESX.GetPlayerFromId(source)
+		if zone == "TraitementVin" then
+			local itemQuantity = xPlayer.getInventoryItem('feuilletab').count
+			
+			if itemQuantity <= 0 then
+				TriggerClientEvent('esx:showNotification', source, _U('not_enough_feuilletab'))
+				return
+			else
+				local rand = math.random(0,100)
+				if (rand >= 98) then
+					SetTimeout(1800, function()
+						xPlayer.removeInventoryItem('feuilletab', 1)
+						xPlayer.addInventoryItem('tabacsec', 1)
+						TriggerClientEvent('esx:showNotification', source, _U('tabacsec'))
+						Transform(source, zone)
+					end)
+				else
+					SetTimeout(1800, function()
+						xPlayer.removeInventoryItem('feuilletab', 1)
+						xPlayer.addInventoryItem('tabacsec', 1)
+				
+						Transform(source, zone)
+					end)
+				end
+			end
+		elseif zone == "TraitementJus" then
+			local itemQuantity = xPlayer.getInventoryItem('feuilletab').count
+			if itemQuantity <= 0 then
+				TriggerClientEvent('esx:showNotification', source, _U('not_enough_feuilletab'))
+				return
+			else
+				SetTimeout(1800, function()
+					xPlayer.removeInventoryItem('feuilletab', 1)
+					xPlayer.addInventoryItem('tabacsec', 1)
+		  
+					Transform(source, zone)	  
+				end)
+			end
+		end
+	end	
+end
+
+RegisterServerEvent('esx_tabacjob:startTransform')
+AddEventHandler('esx_tabacjob:startTransform', function(zone)
+	local _source = source
+  	
+	if PlayersTransforming[_source] == false then
+		TriggerClientEvent('esx:showNotification', _source, '~r~C\'est pas bien de glitch ~w~')
+		PlayersTransforming[_source]=false
+	else
+		PlayersTransforming[_source]=true
+		TriggerClientEvent('esx:showNotification', _source, _U('transforming_in_progress')) 
+		Transform(_source,zone)
+	end
+end)
+
+RegisterServerEvent('esx_tabacjob:stopTransform')
+AddEventHandler('esx_tabacjob:stopTransform', function()
+
+	local _source = source
+	
+	if PlayersTransforming[_source] == true then
+		PlayersTransforming[_source]=false
+		TriggerClientEvent('esx:showNotification', _source, 'Vous sortez de la ~r~zone')
+		
+	else
+		TriggerClientEvent('esx:showNotification', _source, 'Vous pouvez ~g~transformer votre feuilletab')
+		PlayersTransforming[_source]=true
+		
+	end
+end)
+
+local function Sell(source, zone)
+
+	if PlayersSelling[source] == true then
+		local xPlayer  = ESX.GetPlayerFromId(source)
+		
+		if zone == 'SellFarm' then
+			if xPlayer.getInventoryItem('tabacsec').count <= 0 then
+				vine = 0
+			else
+				vine = 1
+			end
+			
+			if xPlayer.getInventoryItem('jus_feuilletab').count <= 0 then
+				jus = 0
+			else
+				jus = 1
+			end
+		
+			if vine == 0 and jus == 0 then
+				TriggerClientEvent('esx:showNotification', source, _U('no_product_sale'))
+				return
+			elseif xPlayer.getInventoryItem('tabacsec').count <= 0 and jus == 0 then
+				TriggerClientEvent('esx:showNotification', source, _U('no_vin_sale'))
+				vine = 0
+				return
+			elseif xPlayer.getInventoryItem('jus_feuilletab').count <= 0 and vine == 0then
+				TriggerClientEvent('esx:showNotification', source, _U('no_jus_sale'))
+				jus = 0
+				return
+			else
+				if (jus == 1) then
+					SetTimeout(1100, function()
+						local money = math.random(18,25)
+						xPlayer.removeInventoryItem('jus_feuilletab', 1)
+						local societyAccount = nil
+
+						TriggerEvent('esx_addonaccount:getSharedAccount', 'society_tabac', function(account)
+							societyAccount = account
+						end)
+						if societyAccount ~= nil then
+							societyAccount.addMoney(money)
+							TriggerClientEvent('esx:showNotification', xPlayer.source, _U('comp_earned') .. money)
+						end
+						Sell(source,zone)
+					end)
+				elseif (vine == 1) then
+					SetTimeout(1100, function()
+						local money = math.random(30,35)
+						xPlayer.removeInventoryItem('vine', 1)
+						local societyAccount = nil
+
+						TriggerEvent('esx_addonaccount:getSharedAccount', 'society_tabac', function(account)
+							societyAccount = account
+						end)
+						if societyAccount ~= nil then
+							societyAccount.addMoney(money)
+							TriggerClientEvent('esx:showNotification', xPlayer.source, _U('comp_earned') .. money)
+						end
+						Sell(source,zone)
+					end)
+				end
+				
+			end
+		end
+	end
+end
+
+RegisterServerEvent('esx_tabacjob:startSell')
+AddEventHandler('esx_tabacjob:startSell', function(zone)
+
+	local _source = source
+	
+	if PlayersSelling[_source] == false then
+		TriggerClientEvent('esx:showNotification', _source, '~r~C\'est pas bien de glitch ~w~')
+		PlayersSelling[_source]=false
+	else
+		PlayersSelling[_source]=true
+		TriggerClientEvent('esx:showNotification', _source, _U('sale_in_prog'))
+		Sell(_source, zone)
+	end
+
+end)
+
+RegisterServerEvent('esx_tabacjob:stopSell')
+AddEventHandler('esx_tabacjob:stopSell', function()
+
+	local _source = source
+	
+	if PlayersSelling[_source] == true then
+		PlayersSelling[_source]=false
+		TriggerClientEvent('esx:showNotification', _source, 'Vous sortez de la ~r~zone')
+		
+	else
+		TriggerClientEvent('esx:showNotification', _source, 'Vous pouvez ~g~vendre')
+		PlayersSelling[_source]=true
+	end
+
+end)
+
+RegisterServerEvent('esx_tabacjob:getStockItem')
+AddEventHandler('esx_tabacjob:getStockItem', function(itemName, count)
+
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	TriggerEvent('esx_addoninventory:getSharedInventory', 'society_tabac', function(inventory)
+
+		local item = inventory.getItem(itemName)
+
+		if item.count >= count then
+			inventory.removeItem(itemName, count)
+			xPlayer.addInventoryItem(itemName, count)
+		else
+			TriggerClientEvent('esx:showNotification', xPlayer.source, _U('quantity_invalid'))
+		end
+
+		TriggerClientEvent('esx:showNotification', xPlayer.source, _U('have_withdrawn') .. count .. ' ' .. item.label)
+
+	end)
+
+end)
+
+ESX.RegisterServerCallback('esx_tabacjob:getStockItems', function(source, cb)
+
+	TriggerEvent('esx_addoninventory:getSharedInventory', 'society_tabac', function(inventory)
+		cb(inventory.items)
+	end)
+
+end)
+
+RegisterServerEvent('esx_tabacjob:putStockItems')
+AddEventHandler('esx_tabacjob:putStockItems', function(itemName, count)
+
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	TriggerEvent('esx_addoninventory:getSharedInventory', 'society_tabac', function(inventory)
+
+		local item = inventory.getItem(itemName)
+
+		if item.count >= 0 then
+			xPlayer.removeInventoryItem(itemName, count)
+			inventory.addItem(itemName, count)
+		else
+			TriggerClientEvent('esx:showNotification', xPlayer.source, _U('quantity_invalid'))
+		end
+
+		TriggerClientEvent('esx:showNotification', xPlayer.source, _U('added') .. count .. ' ' .. item.label)
+
+	end)
+end)
+
+ESX.RegisterServerCallback('esx_tabacjob:getPlayerInventory', function(source, cb)
+
+	local xPlayer    = ESX.GetPlayerFromId(source)
+	local items      = xPlayer.inventory
+
+	cb({
+		items      = items
+	})
+
+end)
+
+
+ESX.RegisterUsableItem('jus_feuilletab', function(source)
+
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	xPlayer.removeInventoryItem('jus_feuilletab', 1)
+
+	TriggerClientEvent('esx_status:add', source, 'hunger', 40000)
+	TriggerClientEvent('esx_status:add', source, 'thirst', 120000)
+	TriggerClientEvent('esx_basicneeds:onDrink', source)
+	TriggerClientEvent('esx:showNotification', source, _U('used_jus'))
+
+end)
+
+ESX.RegisterUsableItem('grand_cru', function(source)
+
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	xPlayer.removeInventoryItem('grand_cru', 1)
+
+	TriggerClientEvent('esx_status:add', source, 'drunk', 400000)
+	TriggerClientEvent('esx_basicneeds:onDrink', source)
+	TriggerClientEvent('esx:showNotification', source, _U('used_grand_cru'))
+
+end)
