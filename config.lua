1a1d2d65aac91b3fc546e2991a38d9699beeee1f
Bruno <samguegvideos@gmail.com>
2018-08-10 19:41:37 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,76 @@
+Config                            = {}
+Config.DrawDistance               = 100.0
+Config.MaxInService               = -1
+Config.EnablePlayerManagement     = true
+Config.EnableSocietyOwnedVehicles = false
+Config.Locale                     = 'fr'
+
+Config.Zones = {
+
+	feuilletabFarm = {
+		Pos   = {x = 268.4737, y = 6478.4584, z = 29.70},
+		Size  = {x = 3.5, y = 3.5, z = 2.0},
+		Color = {r = 136, g = 243, b = 216},
+		Name  = "Récolte de Feuilles",
+		Type  = 27
+	},
+
+
+	TraitementVin = {
+		Pos   = {x = 1543.567, y = 2176.943, z = 77.814},
+		Size  = {x = 3.5, y = 3.5, z = 2.0},
+		Color = {r = 136, g = 243, b = 216},
+		Name  = "Traitement du Tabac",
+		Type  = 27
+	},
+
+--	TraitementJus = {
+--		Pos   = {x = 811.337, y = 2179.402, z = 51.388},
+--		Size  = {x = 3.5, y = 3.5, z = 2.0},
+--		Color = {r = 136, g = 243, b = 216},
+--		Name  = "Traitement du Jus de raisin",
+--		Type  = 1
+--	},
+	
+	SellFarm = {
+		Pos   = {x = -158.737, y = -54.651, z = 53.410},
+		Size  = {x = 3.5, y = 3.5, z = 2.0},
+		Color = {r = 136, g = 243, b = 216},
+		Name  = "Vente des produits",
+		Type  = 27
+	},
+
+	tabacActions = {
+		Pos   = {x = 1088.976, y = -3102.165, z = -39.950},
+		Size  = {x = 1.5, y = 1.5, z = 1.0},
+		Color = {r = 136, g = 243, b = 216},
+		Name  = "Point d'action",
+		Type  = 25
+	},
+	  
+	VehicleSpawner = {
+		Pos   = {x = 1104.695, y = -3099.471, z = -39.999},
+		Size = {x = 1.5, y = 1.5, z = 1.0},
+		Color = {r = 136, g = 243, b = 216},
+		Name  = "Garage véhicule",
+		Type  = 0
+	},
+
+	VehicleSpawnPoint = {
+		Pos   = {x = 408.991, y = 6486.213, z = 28.00},
+		Size  = {x = 1.5, y = 1.5, z = 1.0},
+		Color = {r = 136, g = 243, b = 216},
+		Name  = "Spawn point",
+		Type  = -1
+	},
+
+	VehicleDeleter = {
+		Pos   = {x = 407.823, y = 6496.196, z = 27.00},
+		Size  = {x = 3.0, y = 3.0, z = 1.0},
+		Color = {r = 136, g = 243, b = 216},
+		Name  = "Ranger son véhicule",
+		Type  = 0
+	}
+
+}
+
---
+++ fr_esx_tabacjob.sql
@@ -0,0 +1,27 @@
+INSERT INTO `addon_account` (`name`, `label`, `shared`) VALUES
+	('society_tabac','tabac',1)
+;
+
+INSERT INTO `addon_inventory` (`name`, `label`, `shared`) VALUES
+	('society_tabac','tabac', 1)
+;
+INSERT INTO `datastore` (`name`, `label`, `shared`) VALUES
+	('society_tabac', 'tabac', 1)
+;
+
+INSERT INTO `jobs`(`name`, `label`, `whitelisted`) VALUES
+	('tabac', 'tabac', 1)
+;
+
+INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
+	('tabac',0,'recrue','Intérimaire', 500, '{"tshirt_1":59,"tshirt_2":0,"torso_1":12,"torso_2":5,"shoes_1":7,"shoes_2":2,"pants_1":9, "pants_2":7, "arms":1, "helmet_1":11, "helmet_2":0,"bags_1":0,"bags_2":0,"ears_1":0,"glasses_1":0,"ears_2":0,"glasses_2":0}','{"tshirt_1":0,"tshirt_2":0,"torso_1":56,"torso_2":0,"shoes_1":27,"shoes_2":0,"pants_1":36, "pants_2":0, "arms":63, "helmet_1":11, "helmet_2":0,"bags_1":0,"bags_2":0,"ears_1":0,"glasses_1":0,"ears_2":0,"glasses_2":0}'),
+	('tabac',1,'novice','tabac', 750, '{"tshirt_1":57,"tshirt_2":0,"torso_1":13,"torso_2":5,"shoes_1":7,"shoes_2":2,"pants_1":9, "pants_2":7, "arms":11, "helmet_1":11, "helmet_2":0,"bags_1":0,"bags_2":0,"ears_1":0,"glasses_1":0,"ears_2":0,"glasses_2":0}','{"tshirt_1":0,"tshirt_2":0,"torso_1":56,"torso_2":0,"shoes_1":27,"shoes_2":0,"pants_1":36, "pants_2":0, "arms":63, "helmet_1":11, "helmet_2":0,"bags_1":0,"bags_2":0,"ears_1":0,"glasses_1":0,"ears_2":0,"glasses_2":0}'),
+	('tabac',2,'cdisenior','Chef de chai', 1200, '{"tshirt_1":57,"tshirt_2":0,"torso_1":13,"torso_2":5,"shoes_1":7,"shoes_2":2,"pants_1":9, "pants_2":7, "arms":11, "helmet_1":11, "helmet_2":0,"bags_1":0,"bags_2":0,"ears_1":0,"glasses_1":0,"ears_2":0,"glasses_2":0}','{"tshirt_1":0,"tshirt_2":0,"torso_1":56,"torso_2":0,"shoes_1":27,"shoes_2":0,"pants_1":36, "pants_2":0, "arms":63, "helmet_1":11, "helmet_2":0,"bags_1":0,"bags_2":0,"ears_1":0,"glasses_1":0,"ears_2":0,"glasses_2":0}'),
+	('tabac',3,'boss','Patron', 1600,'{"tshirt_1":57,"tshirt_2":0,"torso_1":13,"torso_2":5,"shoes_1":7,"shoes_2":2,"pants_1":9, "pants_2":7, "arms":11, "helmet_1":11, "helmet_2":0,"bags_1":0,"bags_2":0,"ears_1":0,"glasses_1":0,"ears_2":0,"glasses_2":0}','{"tshirt_1":15,"tshirt_2":0,"torso_1":14,"torso_2":15,"shoes_1":12,"shoes_2":0,"pants_1":9, "pants_2":5, "arms":1, "helmet_1":11, "helmet_2":0,"bags_1":0,"bags_2":0,"ears_1":0,"glasses_1":0,"ears_2":0,"glasses_2":0}')
+;
+
+
+INSERT INTO `items` (`name`, `label`) VALUES
+	('feuilletab', 'Feuille de tabac'),
+	('tabacsec', 'Tabac')
+;
