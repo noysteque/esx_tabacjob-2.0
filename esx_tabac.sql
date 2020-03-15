83b016cae58bc9225592b901c921e67136c933e8
Bruno <samguegvideos@gmail.com>
2020-03-15 02:19:29 +0000

    MaJ Noms

--- esx_tabac.sql
+++ esx_tabac.sql
@@ -1,27 +1,27 @@
 INSERT INTO `addon_account` (name, label, shared) VALUES 
-	('society_tabac','Tabac',1)
+	('society_tabac','Marlboro',1)
 ;
 
 INSERT INTO `datastore` (name, label, shared) VALUES 
-	('society_tabac','Tabac',1)
+	('society_tabac','Marlboro',1)
 ;
 
 INSERT INTO `addon_inventory` (name, label, shared) VALUES 
-	('society_tabac', 'Tabac', 1)
+	('society_tabac', 'Marlboro', 1)
 ;
 
 INSERT INTO `jobs` (`name`, `label`, `whitelisted`) VALUES
-('tabac', 'Tabac', 1);
+('tabac', 'Marlboro', 1);
 
 INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
-  ('tabac',0,'recrue','Tabagiste',400,'{}','{}'),
-  ('tabac',1,'gerant','Gérant',650,'{}','{}'),
-  ('tabac',2,'boss','Patron',800,'{}','{}');
+  ('tabac',0,'recrue','Tabagiste Marlboro',400,'{}','{}'),
+  ('tabac',1,'gerant','Gérant Marlboro',650,'{}','{}'),
+  ('tabac',2,'boss','Patron Marlboro',800,'{}','{}');
 
 INSERT INTO `items` (name, label) VALUES
   ('tabacblond', 'Tabac Blond'),
   ('tabacbrun', 'Tabac Brun'),
   ('tabacblondsec', 'Tabac Blond Séché'),
   ('tabacbrunsec', 'Tabac Brun Séché'),
-  ('malbora', 'Malbora'),
+  ('malbora', 'Marlboro'),
   ('gitanes', 'Gitanes');
